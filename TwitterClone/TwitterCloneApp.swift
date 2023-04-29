817a48470a0e26034cf87932b72c96e00a92e8f8
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-04-29 15:05:14 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by YILDIRIM on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
